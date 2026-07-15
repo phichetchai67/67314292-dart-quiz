658d100630efcab5089aff0fc081f5ea67c26aa8
phichetchai67 <phichetchais67@nu.ac.th>
2026-07-15 11:11:20 +0700

    1

--- q1_basics.dart
+++ q1_basics.dart
@@ -1,6 +1,20 @@
 final vatRate = 0.07;
 void main(){
    final shopName = "Dart Cafe";
-   print("ร้าน : $shopName");
+   print("ร้าน: $shopName");
    print("อัตราภาษี (vatRate): $vatRate");
+   print("---");
+
+   List<String> categories = ["เครื่องดื่ม","ของคาว","ของหวาน"];
+   Map<String,int> menu ={
+    'ลาเต้':55,
+    'ข้าวผัด':60,
+    'ไก่ทอด':35,
+    'เค้ก':80,
+  };
+  print("หมวดเมนู: $categories");
+  print("จำนวนเมนูทั้งหมด: ${menu.length}");
+  print("ราคาลาเต้: ${menu['ลาเต้']} บาท");
+     
+   
 }
